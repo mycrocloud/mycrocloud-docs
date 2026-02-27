5bef95bd433f158565adf7770d36ee95b3d14ce3
Nam Pham <pvnam95@hotmail.com>
2026-02-27 16:55:07 +0900

    Add SVG illustration for "Focus on What Matters"

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'MycroCloud Docs',
+  tagline: '',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.mycrocloud.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mycrocloud', // Usually your GitHub org/user name.
+  projectName: 'mycrocloud-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -50,11 +50,11 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    // image: '',
     navbar: {
-      title: 'My Site',
+      title: 'MycroCloud Docs',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'MycroCloud Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -79,7 +79,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} MycroCloud. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} MycroCloud.`,
     },
     prism: {
       theme: prismThemes.github,
